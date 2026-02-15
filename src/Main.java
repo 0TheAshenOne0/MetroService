d5700a7ef9b031253b0e5b74b12d86bd66c1ac71
0TheAshenOne0 <oaojassim@gmail.com>
2026-02-15 21:45:53 +0000

    Optimised code logic

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,9 @@
-Scanner scanner = new Scanner(System.in);
-
 Metro london = new Metro(5, "TheTube", true);
 
 void main() {
-    int minors = 0;
-    IO.println("Welcome to the London underground metro service! How many passengers shall be boarding?");
-    int members = scanner.nextInt();
-    int[] ages = new int[members];
-    if (Metro.late())
-    {
+    if (Metro.late()) {
         london.availability = false;
     }
-    for (int i = 0; i < members; i++) {
-        IO.println("Age person " + (i + 1) + ":");
-        ages[i] = scanner.nextInt();
-        if (ages[i] < 18)
-        {
-            minors++;
-
-        }
-
-    }
-
-    int adultTickets = members - minors;
-    double totalCost = london.price * adultTickets + (london.price * minors / 2);
-    System.out.println("Adults: " + adultTickets);
-    System.out.println("Minors: " + minors);
-    System.out.println("Train: " + london.name + "\nAvailability Status: " + london.availability) ;
-    if (!london.availability)
-    {
-        System.out.println("It's too late! The metro is closed!");
-        return;
-    }
-    System.out.println("Total Cost: " + totalCost + "$");
-
-
-
+    london.processor();
 
 }
--- src/Metro.java
+++ src/Metro.java
@@ -1,9 +1,12 @@
 import java.time.LocalTime;
+import java.util.Scanner;
 
-public class Metro {
+ class Metro {
     double price;
     String name;
     boolean availability;
+     Scanner scanner = new Scanner(System.in);
+
 
     public Metro(double ticketPrice, String trainName, boolean isAvailable)
     {
@@ -14,8 +17,36 @@ public class Metro {
     }
     static public boolean late()
     {
-        return LocalTime.parse("23:00:00").isBefore(LocalTime.now());
+        return LocalTime.parse("21:00:00").isBefore(LocalTime.now());
 
+    }
+     public void processor()
+    {
+        if (!availability)
+        {
+            System.out.println("It's too late! The metro is closed!");
+            return;
+        }
+        int minors = 0;
+        IO.println("Welcome to the London underground metro service! How many passengers shall be boarding?");
+        int members = scanner.nextInt();
+        int[] ages = new int[members];
+        for (int i = 0; i < members; i++) {
+            IO.println("Age person " + (i + 1) + ":");
+            ages[i] = scanner.nextInt();
+            if (ages[i] < 18)
+            {
+                minors++;
+
+            }
+
+        }
+
+        int adultTickets = members - minors;
+        double totalCost = price * adultTickets + (price * minors / 2);
+        System.out.println("Adults: " + adultTickets);
+        System.out.println("Minors: " + minors);
+        System.out.println("Train: " + name + "\nAvailability Status: " + availability + "\nTotal Cost: " + totalCost + "$") ;
 
 
     }
